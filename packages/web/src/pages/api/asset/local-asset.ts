24e5ef00fc6e06bea124aeba097235e4b1a350e7
Raul <raulgrecio@gmail.com>
2026-03-16 08:47:04 +0100

    refactor: move local asset API endpoint to `/api/asset/local-asset` and update references.

--- packages/web/src/pages/api/asset/local-asset.ts
+++ packages/web/src/pages/api/asset/local-asset.ts
@@ -5,7 +5,8 @@ import fs from 'fs';
 import path from 'path';
 
 import { AssetPathResolver, NodeFileSystem, getMimeType } from '@my-offline-lms/core';
-import { CONFIG_PATH, MONOREPO_ROOT } from '../config/paths';
+
+import { CONFIG_PATH, MONOREPO_ROOT } from '../../../config/paths';
 
 const fsAdapter = new NodeFileSystem();
 const resolver = new AssetPathResolver({
