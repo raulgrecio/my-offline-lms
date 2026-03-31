83f13b6ccaedb317d5725447ca9fd2ecfab9fbc3
Raul <raulgrecio@gmail.com>
2026-03-31 12:47:09 +0200

    refactor: consolidate import statements using barrel files and fix code indentation in ScraperService

--- packages/scraper/src/ScraperService.ts
+++ packages/scraper/src/ScraperService.ts
@@ -6,26 +6,31 @@ import { NodeFileSystem, NodePath, UniversalFileSystem, HttpFileSystem, AssetPat
 import { env } from './config/env';
 import { PLATFORM } from './config/platform';
 import { getAssetPathsConfig, getMonorepoRoot, getAuthState, getAuthDir, getInterceptedDir } from './config/paths';
-import { AssetNamingService } from './features/asset-download/infrastructure/AssetNamingService';
-import { SQLiteAssetRepository } from './features/asset-download/infrastructure/AssetRepository';
-import { DiskAssetStorage } from './features/asset-download/infrastructure/DiskAssetStorage';
-import { YtDlpVideoDownloader } from './features/asset-download/infrastructure/YtDlpVideoDownloader';
-import { DiskAuthSessionStorage } from './features/auth-session/infrastructure/DiskAuthSessionStorage';
-import { SQLiteCourseRepository } from './features/platform-sync/infrastructure/CourseRepository';
-import { DiskInterceptedDataRepositoryFactory } from './features/platform-sync/infrastructure/DiskInterceptedDataRepositoryFactory';
-import { SQLiteLearningPathRepository } from './features/platform-sync/infrastructure/LearningPathRepository';
-import { OraclePlatformUrlProvider } from './features/platform-sync/infrastructure/OraclePlatformUrlProvider';
-import { DownloadCourse } from './features/asset-download/application/DownloadCourse';
-import { DownloadGuides } from './features/asset-download/application/DownloadGuides';
-import { DownloadPath } from './features/asset-download/application/DownloadPath';
-import { DownloadVideos } from './features/asset-download/application/DownloadVideos';
-import { SyncCourse } from './features/platform-sync/application/SyncCourse';
-import { AuthSession } from './features/auth-session/application/AuthSession';
-import { ValidateAuthSession } from './features/auth-session/application/ValidateAuthSession';
-import { SyncLearningPath } from './features/platform-sync/application/SyncLearningPath';
-import { BrowserProvider } from './platform/browser/BrowserProvider';
-import { BrowserInterceptor } from './platform/browser/BrowserInterceptor';
-import { getDb } from './platform/database/database';
+import {
+  AssetNamingService,
+  SQLiteAssetRepository,
+  DiskAssetStorage,
+  YtDlpVideoDownloader,
+  DownloadCourse,
+  DownloadGuides,
+  DownloadPath,
+  DownloadVideos
+} from './features/asset-download';
+import {
+  DiskAuthSessionStorage,
+  AuthSession,
+  ValidateAuthSession
+} from './features/auth-session';
+import {
+  SQLiteCourseRepository,
+  DiskInterceptedDataRepositoryFactory,
+  SQLiteLearningPathRepository,
+  OraclePlatformUrlProvider,
+  SyncCourse,
+  SyncLearningPath
+} from './features/platform-sync';
+import { BrowserProvider, BrowserInterceptor } from './platform/browser';
+import { getDb } from './platform/database';
 
 export interface ScraperServiceConfig {
   keepTempWorkspaces?: boolean;
@@ -143,42 +148,42 @@ export class ScraperService {
 
   async download(id: string, type: DownloadType = 'all', isPath: boolean = false) {
     const deps = await this.init();
-    const assetStorage = new DiskAssetStorage({ 
-        fs: deps.universalFs, 
-        path: deps.nodePath, 
-        resolver: deps.assetPathResolver 
+    const assetStorage = new DiskAssetStorage({
+      fs: deps.universalFs,
+      path: deps.nodePath,
+      resolver: deps.assetPathResolver
     });
-    const videoDownloader = new YtDlpVideoDownloader({ 
-        authSessionStorage: deps.authSessionStorage, 
-        logger: deps.logger 
+    const videoDownloader = new YtDlpVideoDownloader({
+      authSessionStorage: deps.authSessionStorage,
+      logger: deps.logger
     });
 
     const downloadGuides = new DownloadGuides({
-        ...deps,
-        assetStorage,
-        config: {
-            keepTempImages: env.KEEP_TEMP_IMAGES,
-            selectors: {
-                iframe: PLATFORM.SELECTORS.GUIDE.IFRAME,
-                flipbookPages: PLATFORM.SELECTORS.GUIDE.FLIPBOOK_PAGES
-            }
+      ...deps,
+      assetStorage,
+      config: {
+        keepTempImages: env.KEEP_TEMP_IMAGES,
+        selectors: {
+          iframe: PLATFORM.SELECTORS.GUIDE.IFRAME,
+          flipbookPages: PLATFORM.SELECTORS.GUIDE.FLIPBOOK_PAGES
         }
+      }
     });
 
     const downloadVideos = new DownloadVideos({
-        ...deps,
-        courseRepository: deps.courseRepo,
-        assetRepository: deps.assetRepo,
-        assetStorage,
-        videoDownloader,
-        config: {
-            selectors: {
-                video: {
-                    startBtn: PLATFORM.SELECTORS.VIDEO.START_BTN,
-                    playBtn: PLATFORM.SELECTORS.VIDEO.PLAY_BTN
-                }
-            }
+      ...deps,
+      courseRepository: deps.courseRepo,
+      assetRepository: deps.assetRepo,
+      assetStorage,
+      videoDownloader,
+      config: {
+        selectors: {
+          video: {
+            startBtn: PLATFORM.SELECTORS.VIDEO.START_BTN,
+            playBtn: PLATFORM.SELECTORS.VIDEO.PLAY_BTN
+          }
         }
+      }
     });
 
     try {
@@ -188,38 +193,38 @@ export class ScraperService {
 
       if (isPath) {
         const syncCourse = new SyncCourse({
-            ...deps,
-            courseRepository: deps.courseRepo,
-            assetRepository: deps.assetRepo,
-            config: {
-                keepTempWorkspaces: env.KEEP_TEMP_WORKSPACES,
-                selectors: { guidesTab: PLATFORM.SELECTORS.COURSE.GUIDES_TAB },
-                oracleConstants: { videoTypeId: PLATFORM.CONSTANTS.ORACLE.VIDEO_TYPE_ID }
-            }
+          ...deps,
+          courseRepository: deps.courseRepo,
+          assetRepository: deps.assetRepo,
+          config: {
+            keepTempWorkspaces: env.KEEP_TEMP_WORKSPACES,
+            selectors: { guidesTab: PLATFORM.SELECTORS.COURSE.GUIDES_TAB },
+            oracleConstants: { videoTypeId: PLATFORM.CONSTANTS.ORACLE.VIDEO_TYPE_ID }
+          }
         });
         const syncPath = new SyncLearningPath({
-            ...deps,
-            learningPathRepo: deps.pathRepo,
-            courseRepo: deps.courseRepo,
-            syncCourse,
-            config: { keepTempWorkspaces: env.KEEP_TEMP_WORKSPACES }
+          ...deps,
+          learningPathRepo: deps.pathRepo,
+          courseRepo: deps.courseRepo,
+          syncCourse,
+          config: { keepTempWorkspaces: env.KEEP_TEMP_WORKSPACES }
         });
         const downloader = new DownloadPath({
-            learningPathRepo: deps.pathRepo,
-            syncLearningPath: syncPath,
-            downloadGuides,
-            downloadVideos,
-            namingService: deps.namingService,
-            logger: deps.logger,
+          learningPathRepo: deps.pathRepo,
+          syncLearningPath: syncPath,
+          downloadGuides,
+          downloadVideos,
+          namingService: deps.namingService,
+          logger: deps.logger,
         });
         await downloader.execute({ pathInput: id, type });
       } else {
         const downloader = new DownloadCourse({
-            courseRepo: deps.courseRepo,
-            downloadGuides,
-            downloadVideos,
-            namingService: deps.namingService,
-            logger: deps.logger,
+          courseRepo: deps.courseRepo,
+          downloadGuides,
+          downloadVideos,
+          namingService: deps.namingService,
+          logger: deps.logger,
         });
         await downloader.execute({ courseInput: id, type });
       }
